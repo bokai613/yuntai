23fa157febc4a146fbcdac2f2953581c8e20e0bf
黎博凯 <libokai@xforceplus.com>
2023-03-15 18:30:02 +0800

    first commit

---
+++ src/main/java/yuntai/cn/libokai/YuntaiBackendApplication.java
@@ -0,0 +1,13 @@
+package yuntai.cn.libokai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuntaiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuntaiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yuntai/cn/libokai/YuntaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package yuntai.cn.libokai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuntaiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
